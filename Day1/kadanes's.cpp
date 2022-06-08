2fc98b335fe802731c57c4dd9bbdb9a06a1c9527
YASWANTH MUTAKANI <63775530+Yash1heck@users.noreply.github.com>
2022-06-08 20:23:17 +0530

    Update kadanes's.cpp

--- Day1/kadanes's.cpp
+++ Day1/kadanes's.cpp
@@ -23,3 +23,26 @@ long long maxSubarraySum(int arr[], int n)
         Taking input and printing output is handled automatically.
     */
 }
+/********LEETCODE VARIATION***********/
+int maxSubArray(vector<int>& nums) {
+        
+    int sum=0;
+    int max_till=0;
+        for(int i=0;i<nums.size();++i)
+        {
+            if(sum+nums[i]>max_till)
+            { 
+             
+                max_till=sum+nums[i];
+                
+            }
+               sum+=nums[i];
+            if(sum<0)
+                sum=0;
+        }
+        if(max_till==0)
+            return *max_element(nums.begin(),nums.end());
+            
+    return max_till;
+   
+}
