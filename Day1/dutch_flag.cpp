bb597d873eb5d4fa7b3f91f6737eee083c680db0
YASWANTH MUTAKANI <63775530+Yash1heck@users.noreply.github.com>
2022-06-08 22:07:28 +0530

    Create dutch_flag.cpp

---
+++ Day1/dutch_flag.cpp
@@ -0,0 +1,26 @@
+void sort012(int *arr, int n)
+{
+   //   Write your code here
+    int low=0,mid=0,high=n-1;
+    while(mid<=high)
+    {
+        if(arr[mid]==0)
+        { 
+             swap(arr[mid++],arr[low++]);
+
+        }
+        else if(arr[mid]==1)
+        {
+           
+                mid+=1;
+        }
+        else if(arr[mid]==2)
+        { 
+             swap(arr[mid],arr[high--]);
+         
+     
+            
+            
+        }
+    }
+}
